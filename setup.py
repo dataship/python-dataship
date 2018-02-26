68e5e72f1e7af23b1ca9aa6de24b419c586cb434
Waylon Flinn <waylonflinn@gmail.com>
2018-02-26 16:20:57 -0600

    new version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ setup(
   name = 'dataship',
   packages = ['dataship', 'dataship.beam'],
   install_requires=['numpy', 'pandas'],
-  version = '0.4.2',
+  version = '0.5.0',
   description = 'Lightweight tools for reading, writing and storing data, locally and over the internet.',
   author = 'Waylon Flinn',
   author_email = 'waylonflinn@gmail.com',
