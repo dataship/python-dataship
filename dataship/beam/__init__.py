606c70c4eaa7f064be473ef5e02e849cce7ea702
Waylon Flinn <waylonflinn@gmail.com>
2018-04-24 10:17:28 -0500

    make pandas reading method available

--- dataship/beam/__init__.py
+++ dataship/beam/__init__.py
@@ -1,5 +1,6 @@
 from .beam import load
 from .beam import read
 from .beam import write
-from .beam import dataframe
-__all__ = ['load', 'read', 'write', 'dataframe']
+from .beam import to_dataframe
+from .beam import from_dataframe
+__all__ = ['load', 'read', 'write', 'to_dataframe', 'from_dataframe']
