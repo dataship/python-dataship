39c923cf1e310729bd4fc16f6e9ef3997eba7802
Waylon Flinn <waylonflinn@gmail.com>
2019-05-08 16:32:52 -0500

    add test for writing and reading

--- dataship/test/test_beam.py
+++ dataship/test/test_beam.py
@@ -2,11 +2,14 @@
 import sys
 import unittest
 import numpy as np
+import os
+import shutil
 
 sys.path.append('../../')
 from dataship import beam
 
-DATA_DIR = "./data/0001/";
+BASE_DIR = "./data/"
+DATA_DIR = BASE_DIR + "0001/";
 
 INDEX = {
 	"doric" : "doric.f32",
@@ -15,8 +18,9 @@ INDEX = {
 };
 
 # close comparison constants
-RTOL = 1e-05,
-ATOL = 1e-07;
+RTOL = 1e-05
+ATOL = 1e-07
+
 
 class TestBeam(unittest.TestCase):
 
@@ -94,6 +98,40 @@ class TestBeam(unittest.TestCase):
 		self.assertTrue(np.isclose(composite[0], 8956, RTOL, ATOL))
 		self.assertTrue(np.isclose(composite[37], 13, RTOL, ATOL))
 		self.assertTrue(np.isclose(composite[78], 78, RTOL, ATOL))
+
+	def test_write_and_read(self):
+
+		test_columns = {
+			"test_a" : np.zeros(100, dtype="uint8"),
+			"test_b" : np.zeros(100, dtype="float32"),
+			"test_c" : np.zeros(100, dtype="int32"),
+			"test_d" : list(range(0, 100)),
+			"test_e" : [""] * 100
+		}
+
+		test_columns["test_a"][11] = 53
+		test_columns["test_b"][67] = 3.1415
+		test_columns["test_c"][2] = 66532
+		test_columns["test_e"][45] = "hello"
+
+		write_dir = BASE_DIR  + "9999/"
+		if os.path.exists(write_dir):
+			shutil.rmtree(write_dir)
+
+		os.makedirs(write_dir)
+
+
+		beam.write(write_dir, test_columns)
+
+		(columns, keys) = beam.read(write_dir)
+
+		for column_name in test_columns.keys():
+
+			self.assertTrue(column_name in columns)
+			if(type(test_columns[column_name]) == np.ndarray):
+				self.assertTrue(np.allclose(test_columns[column_name], columns[column_name]))
+			elif(type(test_columns[column_name]) == list):
+				self.assertTrue(test_columns[column_name] == columns[column_name])
 
 #
 # tape("keys have correct values", function(t){
