55cc79025001f4f6567d764fbb8e1139fcd32d9f
Waylon Flinn <waylonflinn@gmail.com>
2019-05-08 20:31:00 -0500

    publish new version

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ setup(
   name = 'dataship',
   packages = ['dataship', 'dataship.beam'],
   install_requires=['numpy', 'pandas'],
-  version = '0.6.1',
+  version = '0.7.0',
   description = 'Lightweight tools for reading, writing and storing data, locally and over the internet.',
   author = 'Waylon Flinn',
   author_email = 'waylonflinn@gmail.com',
   url = 'https://github.com/dataship/python-dataship', # use the URL to the github repo
-  keywords = ['numpy', 'pandas', 'data-science', 'javascript', 'interoperability', 'columnar', 'data'], # arbitrary keywords
+  keywords = ['numpy', 'pandas', 'data-science', 'javascript', 'interoperability', 'columnar', 'data', 'compressed'], # arbitrary keywords
   classifiers = [],
 )
